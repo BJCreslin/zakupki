f3321fde956c851e39257f95f22fafdbddfc800b
bjcreslin <bjcreslin@gmail.com>
2019-12-02 17:26:58 +0700

    Описание полей DTO файла

--- src/main/java/ru/bjcreslin/zakupki/DTO/PurchaseRegion70.java
+++ src/main/java/ru/bjcreslin/zakupki/DTO/PurchaseRegion70.java
@@ -6,19 +6,33 @@ import java.math.BigDecimal;
 
 @Data
 public class PurchaseRegion70 {
+    //Полное наименование
     private String customerName;
+    //ИНН
     private Integer inn;
+    //Адрес места нахождения
     private String adress;
+    //Наименование
     private String purchaseName;
+    //Статус
     private String Status;
+    //Статус
     private BigDecimal purchaseCost;
+    //ФИО контактного лица
     private String contactPerson;
+    //Контактный телефон
     private String contactPhone;
+    //Сроки поставки
     private String deliveryDate;
+    //Место поставки
     private String deliveryPlace;
+    //Условия оплаты
     private String termsOfPayment;
+    //Описание
     private String description;
+    //Дата окончания подачи предложений
     private String filingDate;
+    //Плановая дата заключения контракта
     private String contractDate;
 
 }
