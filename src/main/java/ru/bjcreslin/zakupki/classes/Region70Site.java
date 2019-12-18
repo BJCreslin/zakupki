0865a40b4558530a4749135a7842728832b15da4
bjcreslin <bjcreslin@gmail.com>
2019-12-18 11:35:23 +0700

    коммент

--- src/main/java/ru/bjcreslin/zakupki/classes/Region70Site.java
+++ src/main/java/ru/bjcreslin/zakupki/classes/Region70Site.java
@@ -1,7 +1,12 @@
 package ru.bjcreslin.zakupki.classes;
 
+/**
+ * Класс для объекта - сайт region70.ru
+ */
 public class Region70Site {
+    //Часть адреса, для поиска
     private static String partOfWebAddress = "/Trade/ViewTrade?id=";
+    //Адрес сайта
     private static String url = "https://region70.rts-tender.ru";
 
     public static String getUrl() {
